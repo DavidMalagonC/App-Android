8206dd1e1cff7c4f889dd2594971d095d251e502
Laura Correa <lauradanicorrea08@outlook.com>
2021-07-20 21:44:45 -0500

    fix/getREquestOwner

---
+++ app/src/main/java/ted/example/proyecto/BusinessmanDashboard.java
@@ -0,0 +1,49 @@
+package ted.example.proyecto;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OwnerDashboard extends AppCompatActivity implements View.OnClickListener{
+
+    User user;
+
+    public void requestLoad(View view) {
+        Intent intent = new Intent(this, Owner.class);
+        user = (User) getIntent().getExtras().getSerializable("user");
+        intent.putExtra("user", user);
+        this.startActivity(intent);
+    }
+
+    public void getRequestLoadInProcess(View view) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboardowner);
+
+    }
+
+    public void getRequestLoadEnd(View view) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public void associateVehicle(View view) {
+        Intent intent = new Intent(this, AsociateVehicle.class);
+        user = (User) getIntent().getExtras().getSerializable("user");
+        intent.putExtra("user", user);
+        this.startActivity(intent);
+
+    }
+}
